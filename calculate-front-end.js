0bf089e0326ea1c767483c9f657530c6a297f54d
Timothy Do <tydo83@gmail.com>
2020-10-22 14:51:58 -0400

    still working on stretch goals

--- calculate-front-end.js
+++ calculate-front-end.js
@@ -1,3 +1,5 @@
+//without stretch goals
+
 const calculateFront = require('./calculate-back-end');
 
 const num1 = process.argv[2];
@@ -5,6 +7,20 @@ const num2 = process.argv[4];
 const operator = process.argv[3];
 const result = calculateFront(num1, num2, operator);
 
-// console.log(result);
+console.log(result);
 
 // Stretch Goals
+// const inputs = [].slice(0);
+// for(i = 2; i < process.argv.length; i++) {
+//         inputs.push(process.argv[i]);
+//     }
+
+
+
+
+
+
+
+
+
+
